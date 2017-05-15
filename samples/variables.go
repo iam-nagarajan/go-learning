358addacaa4defa225ccd31d0206562d6aeccb54
Nagarajan <nselvaraj@owler.com>
2017-05-15 18:57:47 +0530

    Constants learning

--- samples/variables.go
+++ samples/variables.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const con = 15
+
 func main() {
 	var str1 string = "Test"
 	var str2 = "1"
@@ -9,4 +11,7 @@ func main() {
 	var str string
 	var f = 7.0
 	fmt.Printf("str1 = %T, str2 = %T, int1 val = %d, str val = %s, f = %T\n", str1, str2, int1, str, f)
+	fmt.Println(con)
+	//con = 16 // Cannot assign to const
+	//fmt.Println(con)
 }
